17a17f29008353551adbfb6bc1ace5020c0f47d6
mostafizur-raahman <mostafizurrahman0202@gmail.com>
2023-05-21 00:21:56 +0600

    updated route dome

--- src/MyToys/MyToys.jsx
+++ src/MyToys/MyToys.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { FaCross } from "react-icons/fa";
 import { Link, useLoaderData } from "react-router-dom";
 
@@ -5,6 +6,7 @@ import { Link, useLoaderData } from "react-router-dom";
 const MyToys = () => {
     const data = useLoaderData();
     console.log(data);
+    const [remaining, setRemaining] = useState(data);
 
     const handledelete = (id) => {
         console.log(id);
@@ -14,6 +16,8 @@ const MyToys = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data);
+                const remain = remaining.filter(r => r._id !== id);
+                setRemaining(remain)
             })
     }
     return (
@@ -36,14 +40,20 @@ const MyToys = () => {
                     <tbody>
 
                         {
-                            data?.map(toy => <tr key={toy._id}>
+                            remaining?.map(toy => <tr key={toy._id}>
                                 <td>{toy.name}</td>
                                 <td>{toy.sellerName}</td>
                                 <td>{toy.category}</td>
                                 <td>{toy.price}</td>
                                 <td>{toy.rating}</td>
                                 <td>{toy.quantity}</td>
-                                <button className="btn" onClick={() => handledelete(toy._id)}> delete </button>
+
+                                <td className="flex gap-3">
+                                    <Link to={`/update/${toy._id}`}>
+                                        <button className="btn bg-sky-500">update</button>
+                                    </Link>
+                                    <button className="btn bg-red-600" onClick={() => handledelete(toy._id)}> delete </button>
+                                </td>
 
                             </tr>
 
